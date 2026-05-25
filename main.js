de671993e3ab8e5fca247597235c3b0b190596e3
ASharmaNYC3190 <anjalinyc39@gmail.com>
2026-05-25 10:35:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+canvas.width = 800;
+canvas.height = 600;
+
+function gameLoop() {
+    ctx.fillStyle = "#111";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(380, 280, 40, 40);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
