deab31bc52e8263f695e0831c65a11ad843c9639
ASharmaNYC3190 <anjalinyc39@gmail.com>
2026-05-28 22:06:30 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,21 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+canvas.width = 960;
+canvas.height = 640;
+
+const TILE_SIZE = 32;
+
+// CAMERA
+const camera = {
+    x: 0,
+    y: 0
+};
+
+// KEYS
+const keys = {};
+
+// DAY/NIGHT
+let darkness = 0;
+let nightDirection = 0.0004;
+let flashlightOn = true;
